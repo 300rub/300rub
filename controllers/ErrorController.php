161b838fd847a731947016a69c90781676715d0f
Mikhail Vasilyev <m.vasilyev@likefifa.ru>
2014-11-24 18:53:47 +0300

    Контроллер ошибок

---
+++ controllers/ErrorController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace controllers;
+
+use system\base\Controller;
+
+/**
+ * Файл класса ErrorController
+ *
+ * @package controllers
+ */
+class ErrorController extends Controller
+{
+
+	/**
+	 * Шаблон
+	 *
+	 * @var string
+	 */
+	public $layout = "error";
+
+	/**
+	 * Название директории для представлений
+	 *
+	 * @return string
+	 */
+	protected function getViewsDir()
+	{
+		return "error";
+	}
+
+	/**
+	 * Выводит страницу ошибки
+	 *
+	 * @param string $message    сообщение
+	 * @param int    $statusCode статус
+	 * @param string $trace      уровки
+	 */
+	public function actionError($message, $statusCode = 500, $trace = "")
+	{
+		header("HTTP/1.0 {$statusCode}");
+
+		$this->render(
+			"error",
+			array(
+				"statusCode" => $statusCode,
+				"message"    => $message,
+				"trace"      => $trace
+			)
+		);
+	}
+}
