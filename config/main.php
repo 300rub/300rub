f564cd84ffdcf44f6d566670bdcf1109f5fa6f9e
Mikhail Vasilyev <m.vasilyev@likefifa.ru>
2014-11-24 18:57:43 +0300

    Настройки

---
+++ config/main.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Настройки
+ */
+
+return array(
+	"isDebug" => true,
+	"rootDir" => __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR,
+	"baseUrl" => "/",
+	"db"      => array(
+		"host"     => "localhost",
+		"user"     => "itnew",
+		"password" => "mypasS77",
+		"base"     => "_sites",
+		"charset"  => "utf8",
+	),
+);
