c71bf5571f45098fc69fd428f7031b71f6144f6a
Mikhail Vasilyev <m.vasilyev@likefifa.ru>
2014-12-04 14:03:58 +0300

    Тесты

---
+++ tests/fixtures/seo.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	array(
+		"id"          => 1,
+		"name"        => "Название 1",
+		"url"         => "url-1",
+		"title"       => "Заголовок 1",
+		"keywords"    => "Ключевые слова 1",
+		"description" => "Описание 1",
+	)
+);
