416cd0ce26be95a2e67882d444cce3cd3020d785
Mikhail Vasilyev <m.vasilyev@likefifa.ru>
2014-12-18 13:35:50 +0300

    Установка сайта

--- system/web/Web.php
+++ system/web/Web.php
@@ -3,6 +3,9 @@
 namespace system\web;
 
 use system\base\Application;
+use system\db\Db;
+use system\base\Exception;
+use system\base\Language;
 
 /**
  * Файл класса Web.
@@ -23,11 +26,33 @@ class Web extends Application
 	{
 		$startTime = microtime(true);
 
-
+		$this->_setSite();
 
 		$time = number_format(microtime(true) - $startTime, 3);
 		if ($this->config->isDebug) {
 			echo "<script>console.log(\"Время выполнения скрипта: {$time} сек.\");</script>";
 		}
+	}
+
+	private function _setSite()
+	{
+		$host = $_SERVER['HTTP_HOST'];
+		if (substr($host, 0, 4) == "www.") {
+			$host = substr($host, 4);
+		}
+		if (!$host) {
+			throw new Exception(Language::t("common", "Failed to detect host"));
+		}
+
+		$site = Db::fetch("SELECT * FROM sites WHERE host = ?", array($host));
+		if (!$site) {
+			throw new Exception(Language::t("common", "Failed to detect site"));
+		}
+
+		if (!Db::setPdo($site["db_user"], $site["db_password"], $site["db_name"])) {
+			throw new Exception(Language::t("common", "Failed to connect to db"));
+		}
+
+		echo 123;
 	}
 }
