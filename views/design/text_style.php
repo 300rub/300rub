cef87d05eadd16b20b8c6904593e026c1b7c6473
Mikhail <donvasilion@gmail.com>
2015-10-13 00:07:25 +0400

    .

--- views/design/text_style.php
+++ views/design/text_style.php
@@ -8,9 +8,9 @@ echo $model->size ? " font-size: {$model->size}px;" : "";
 echo $model->color ? " color: {$model->color};" : "";
 echo $model->is_italic ? " font-style: italic;" : "";
 echo $model->is_bold ? " font-weight: bold;" : "";
-echo $model->getTextAlign() ? " text-align: " . $model->getTextAlign() . ";" : "";
-echo $model->getTextDecoration() ? " text-decoration: " . $model->getTextDecoration() . ";" : "";
-echo $model->getTextTransform() ? " text-transform: " . $model->getTextTransform() . ";" : "";
+echo $model->align ? " text-align: " . $model->getTextAlign() . ";" : "";
+echo $model->decoration ? " text-decoration: " . $model->getTextDecoration() . ";" : "";
+echo $model->transform ? " text-transform: " . $model->getTextTransform() . ";" : "";
 echo $model->letter_spacing ? " letter-spacing: {$model->letter_spacing}px;" : "";
-echo $model->line_height ? " line-height: {$model->line_height}%" : "";
+echo ($model->line_height && $model->line_height != 100) ? " line-height: {$model->line_height}%;" : "";
 ?>
