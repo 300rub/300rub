493d2593edf05b13691cbc5269bb59f511d2b316
Mikhail Vasilev <vasilev_igorevich@thbs.com>
2015-12-02 16:50:01 +0000

    .

--- c
+++ c
@@ -3,13 +3,9 @@
 
 use system\App;
 
-/**
- * Воспроизводит команду
- *
- * Указывается путь до файла с настройками и передается в команду
- */
-
+// Config file path
 $config = require(__DIR__ . "/config/main.php");
 
+// Runs a command
 require(__DIR__ . "/system/App.php");
 App::console($config)->run();
--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 <?php
 
-// Roxy Taggart
-//kitty bella
-// bijou http://www.pornhd.com/pornstar/bijou
-// erika vution http://www.pornhd.com/videos/16602/fucking-his-first-black-ass-hd-porn-video  http://www.pornhd.com/pornstar/erika-vution
-
 use system\App;
 
-/**
- * Запускает web приложение
- *
- * Указывается путь до файла с настройками и передается в команду
- */
-
+// Config file path
 $config = require(__DIR__ . "/config/main.php");
 
+// Runs the web application
 require(__DIR__ . "/system/App.php");
 App::web($config)->run();
