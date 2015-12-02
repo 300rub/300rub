f7b23b0016560444d134a46f6f004e28a114b92a
Mikhail Vasilev <vasilev_igorevich@thbs.com>
2015-12-02 17:23:11 +0000

    .

--- commands/BuildCommand.php
+++ commands/BuildCommand.php
@@ -7,125 +7,123 @@ use system\base\Logger;
 use system\console\Command;
 
 /**
- * Файл класса BuildCommand
- *
- * Производит сборку
+ * Builds a repository
  *
  * @package commands
  */
 class BuildCommand extends Command
 {
 
-	/**
-	 * Префикс для релиза
-	 */
-	const RELEASE_PREFIX = "release/";
+    /**
+     * Prefix for release GIT branch
+     */
+    const RELEASE_PREFIX = "release.";
 
-	/**
-	 * Применяемая ветка
-	 *
-	 * @var string
-	 */
-	private $_branch = "master";
+    /**
+     * Name of GIT branch for build
+     *
+     * @var string
+     */
+    private $_branch = "master";
 
-	/**
-	 * Ветка для отката
-	 *
-	 * @var string
-	 */
-	private $_prevBranch = "master";
+    /**
+     * Name of GIT branch for rollback
+     *
+     * @var string
+     */
+    private $_prevBranch = "master";
 
-	/**
-	 * Выполняет команду
-	 *
-	 * @param string[] $args аргументы
-	 *
-	 * @return bool
-	 */
-	public function run($args)
-	{
-		if (!$this->_checkFolders()) {
-			exit();
-		}
+    /**
+     * Runs the command
+     *
+     * @param string[] $args command arguments
+     *
+     * @return bool
+     */
+    public function run($args)
+    {
+        if (!$this->_checkFolders()) {
+            exit();
+        }
 
-		$this->_setBranches($args);
+        $this->_setBranches($args);
 
-		Logger::log("Началась сборка из ветки {$this->_branch}", Logger::LEVEL_INFO, "console.build");
-		$this->_gitCheckout($this->_branch);
+        Logger::log("Building from branch {$this->_branch}", Logger::LEVEL_INFO, "console.build");
+        $this->_gitCheckout($this->_branch);
 
-		$migrateCommand = new MigrateCommand;
-		$publicStaticCommand = new PublicStaticCommand;
+        $migrateCommand = new MigrateCommand;
+        $publicStaticCommand = new PublicStaticCommand;
 
-		if (!$migrateCommand->run($args) || !$publicStaticCommand->run($args)) {
-			Logger::log("Во время сборки произошла ошибка", Logger::LEVEL_INFO, "console.build");
+        if (!$migrateCommand->run($args) || !$publicStaticCommand->run($args)) {
+            Logger::log("Errors during building", Logger::LEVEL_INFO, "console.build");
 
-			$this->_gitCheckout($this->_prevBranch);
-			Logger::log("Откатано из ветки {$this->_prevBranch}", Logger::LEVEL_INFO, "console.build");
+            $this->_gitCheckout($this->_prevBranch);
+            Logger::log("Rollback to branch {$this->_prevBranch}", Logger::LEVEL_INFO, "console.build");
 
-			return false;
-		}
+            return false;
+        }
 
-		Logger::log("Сборка успешно завершена", Logger::LEVEL_INFO, "console.build");
-		return true;
-	}
+        Logger::log("Build completed successfully", Logger::LEVEL_INFO, "console.build");
+        return true;
+    }
 
-	/**
-	 * Устанавливает ветки
-	 *
-	 * @param string[] $args аргументы
-	 *
-	 * @return void
-	 */
-	private function _setBranches($args)
-	{
-		if (App::console()->config->isDebug) {
-			if (!empty($args[0])) {
-				$this->_branch = $args[0];
-			}
-		} else {
-			$this->_branch = self::RELEASE_PREFIX . App::console()->config->release->current;
-			$this->_prevBranch = self::RELEASE_PREFIX . App::console()->config->release->prev;
-		}
-	}
+    /**
+     * Sets names of GIT branches for build and rollback
+     *
+     * @param string[] $args command arguments
+     *
+     * @return void
+     */
+    private function _setBranches($args)
+    {
+        if (App::console()->config->isDebug) {
+            if (!empty($args[0])) {
+                $this->_branch = $args[0];
+            }
+        } else {
+            $this->_branch = self::RELEASE_PREFIX . App::console()->config->release->current;
+            $this->_prevBranch = self::RELEASE_PREFIX . App::console()->config->release->prev;
+        }
+    }
 
-	/**
-	 * Проверяет папки для хранения на существование
-	 *
-	 * @return bool
-	 */
-	private function _checkFolders()
-	{
-		$logs = App::console()->config->rootDir . DIRECTORY_SEPARATOR . "logs";
-		if (!file_exists($logs) && !mkdir($logs, 0777)) {
-			echo "	> Невозможно создать папку \"logs\"\n";
-			return false;
-		}
+    /**
+     * Checks basic folders
+     *
+     * @return bool
+     */
+    private function _checkFolders()
+    {
+        $logs = App::console()->config->rootDir . DIRECTORY_SEPARATOR . "logs";
+        if (!file_exists($logs) && !mkdir($logs, 0777)) {
+            echo "	> Unable to create folder \"logs\"\n";
+            return false;
+        }
 
-		$backups = App::console()->config->rootDir . DIRECTORY_SEPARATOR . "backups";
-		if (!file_exists($backups) && !mkdir($backups, 0777)) {
-			Logger::log("Невозможно создать папку \"backups\"", Logger::LEVEL_ERROR, "console.build");
-			return false;
-		}
+        $backups = App::console()->config->rootDir . DIRECTORY_SEPARATOR . "backups";
+        if (!file_exists($backups) && !mkdir($backups, 0777)) {
+            Logger::log("Unable to create folder \"backups\"", Logger::LEVEL_ERROR, "console.build");
+            return false;
+        }
 
-		$vendors = App::console()->config->rootDir . DIRECTORY_SEPARATOR . "vendors";
-		if (!file_exists($vendors) && !mkdir($vendors, 0777)) {
-			Logger::log("Невозможно создать папку \"vendors\"", Logger::LEVEL_ERROR, "console.build");
-			return false;
-		}
+        $vendors = App::console()->config->rootDir . DIRECTORY_SEPARATOR . "vendors";
+        if (!file_exists($vendors) && !mkdir($vendors, 0777)) {
+            Logger::log("Unable to create folder \"vendors\"", Logger::LEVEL_ERROR, "console.build");
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Производит сборку ветки
-	 *
-	 * @param string $branch название ветки
-	 *
-	 * @return bool
-	 */
-	private function _gitCheckout($branch)
-	{
-		$command = "
+    /**
+     * Builds GIT branch
+     *
+     * @param string $branch GIT branch name
+     *
+     * @return bool
+     */
+    private function _gitCheckout($branch)
+    {
+        $command = "
 			git add .;
 			git reset --hard;
 			git fetch --all -p;
@@ -134,8 +132,8 @@ class BuildCommand extends Command
 			chmod 777 c;
 			php /root/composer.phar update;
 		";
-		exec($command);
+        exec($command);
 
-		return true;
-	}
+        return true;
+    }
 }
