8144a47818404feb09b73d1612c02e616ab966da
Mikhail Vasilev <vasilev_igorevich@thbs.com>
2015-12-17 13:46:34 +0000

    .

--- system/base/Application.php
+++ system/base/Application.php
@@ -5,39 +5,93 @@ namespace system\base;
 use system\db\Db;
 use system\web\Language;
 
+/**
+ * Abstract class for work with application
+ *
+ * @package system.base
+ */
 abstract class Application
 {
 
 	/**
-	 * Настройки
+	 * Settings from config
 	 *
 	 * @var object
 	 */
 	public $config = null;
 
 	/**
-	 * Запускает команду
+	 * Runs application
 	 *
 	 * @return void
 	 */
 	abstract public function run();
 
 	/**
-	 * Конструктор
+	 * Constructor
 	 *
-	 * @throws Exception
-	 *
-	 * @param array $config конфиг
+	 * @param array $config Settings from config
 	 */
 	public function __construct($config)
 	{
+		$this
+			->_setErrorHandler()
+			->_parseConfig($config)
+			->_activateVendorAutoload()
+			->_checkDbConnection();
+	}
+
+	/**
+	 * Sets Error Handler
+	 *
+	 * @return Application
+	 */
+	private function _setErrorHandler()
+	{
 		new ErrorHandler();
 
+		return $this;
+	}
+
+	/**
+	 * Parses config settings
+	 *
+	 * @param array $config Config settings
+	 *
+	 * @return Application
+	 */
+	private function _parseConfig($config)
+	{
 		$this->config = json_decode(json_encode($config));
+
+		return $this;
+	}
+
+	/**
+	 * Activates vendor autoload
+	 *
+	 * @return Application
+	 */
+	private function _activateVendorAutoload()
+	{
 		require_once(__DIR__ . "/../../vendors/autoload.php");
 
+		return $this;
+	}
+
+	/**
+	 * Checks connection with DB
+	 *
+	 * @return Application
+	 *
+	 * @throws Exception
+	 */
+	private function _checkDbConnection()
+	{
 		if (!Db::setPdo($this->config->db->user, $this->config->db->password, $this->config->db->name)) {
 			throw new Exception(Language::t("default", "Не удалось соединиться с базой данных"));
 		}
+
+		return $this;
 	}
 }
