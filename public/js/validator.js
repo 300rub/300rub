5d2829ebef9a0502e0eb5cb4e3c9c0e73400d7d0
Mikhail Vasilev <vasilev_igorevich@thbs.com>
2016-01-26 13:22:08 +0000

    .

--- public/js/validator.js
+++ public/js/validator.js
@@ -1,25 +1,56 @@
 !function ($, c) {
 	"use strict";
 
-	c.Validator = function (form) {
-		this.$form = form;
+	/**
+	 * Object for working with form's validation
+	 *
+	 * @param    {HTMLElement} [$form] DOM-element of form
+	 * @property {HTMLElement} [$form] DOM-element of form
+	 *
+	 * @constructor
+	 */
+	c.Validator = function ($form) {
+		this.$form = $form;
 	};
 
+	/**
+	  * Validator prototype
+	  */
 	c.Validator.prototype = {
+		/**
+		 * Constructor
+		 *
+		 * @var {window.Core.Validator}
+		 */
+		constructor: c.Validator,
+
+		/**
+		 * Errors
+		 *
+		 * @var {Object}
+		 */
 		_errors: {},
 
+		/**
+		 * Validates form
+		 *
+		 * @returns {Boolean}
+         */
 		validate: function() {
+			var methodName, $field, split;
 			this._errors = {};
 
-			var $field, split;
 			this.$form.find(".j-validate").each($.proxy(function (i, field) {
 				$field = $(field);
 				$.each($field.data("rules").split(", "), $.proxy(function (i, rule) {
 					split = rule.split("-");
-					if (split[1] === undefined) {
-						this[split[0]]($field);
-					} else {
-						this[split[0]]($field, split[1]);
+					methodName = "_" + split[0];
+					if (this[methodName] !== undefined) {
+						if (split[1] === undefined) {
+							this[methodName]($field);
+						} else {
+							this[methodName]($field, split[1]);
+						}
 					}
 				}, this));
 			}, this));
@@ -27,40 +58,82 @@
 			return this.showErrors(this._errors);
 		},
 
-		required: function ($field) {
-			if ($.trim($field.val()) === "") {
-				this.addError($field.attr("name"), "required");
-			}
-		},
-
-		max: function ($field, max) {
-			if ($.trim($field.val()).length > parseInt(max)) {
-				this.addError($field.attr("name"), "max");
-			}
-		},
-
-		addError: function (key, value) {
-			if (this._errors[key] === undefined) {
-				this._errors[key] = value;
-			}
-		},
-
+		/**
+		 * Shows errors
+		 *
+		 * @param {Object} [errors] Errors
+		 *
+		 * @returns {Boolean}
+         */
 		showErrors: function (errors) {
 			this.$form.find("div.j-error").remove();
 
 			if ($.isEmptyObject(errors)) {
 				return true;
 			}
-			
+
 			$.each(errors, $.proxy(function (name, error) {
 				this.$form.find(name.nameToClass()).after(c.$templates.find(".j-error-" + error).clone());
 			}, this));
-			
+
 			return false;
+		},
+
+		/**
+		 * Checks for requiring
+		 *
+		 * @param {HTMLElement} [$field] Field
+		 *
+         * @private
+         */
+		_required: function ($field) {
+			if ($.trim($field.val()) === "") {
+				this._addError($field.attr("name"), "required");
+			}
+		},
+
+		/**
+		 * Checks for max value
+		 *
+		 * @param {HTMLElement} [$field] Field
+		 * @param {Integer}     [max]    Max value
+		 *
+         * @private
+         */
+		_max: function ($field, max) {
+			if ($.trim($field.val()).length > parseInt(max)) {
+				this._addError($field.attr("name"), "max");
+			}
+		},
+
+		/**
+		 * Adds error
+		 *
+		 * @param {String} [key]   Key
+		 * @param {String} [value] Value
+		 *
+         * @private
+         */
+		_addError: function (key, value) {
+			if (this._errors[key] === undefined) {
+				this._errors[key] = value;
+			}
 		}
 	};
 
-	$.validator = function (form) {
-		return new c.Validator(form);
+	/**
+	 * Adds Validator to jquery
+	 *
+	 * @returns {Window.Core.Validator}
+	 */
+	$.validator = function ($form) {
+		return new c.Validator($form);
 	};
+
+	/**
+	 * Validator constructor
+	 *
+	 * @constructor
+	 */
+	$.validator.Constructor = c.Validator;
 }(window.jQuery, window.Core);
--- public/js/window.login.js
+++ public/js/window.login.js
@@ -1,6 +1,9 @@
 !function ($, c) {
 	"use strict";
 
+	/**
+	 * Window login handler
+	 */
 	c.Window.prototype.login = function() {
 		var $template = c.$templates.find(".j-window-login-container").clone();
 		$template.appendTo(this.$window.find(".j-container"));
