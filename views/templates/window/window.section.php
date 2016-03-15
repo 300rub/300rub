fd21928a8be1f13096a14f36c8e08de2f087e56d
Mikhail Vasilev <vasilev_igorevich@thbs.com>
2016-03-15 13:50:20 +0000

    .

--- views/templates/window/window.section.php
+++ views/templates/window/window.section.php
@@ -4,9 +4,9 @@ use system\web\Language;
 
 <div class="j-window-section-container"></div>
 
-<div class="j-window-section-line l-hide">
-    <div class="j-header">
-        <div class="j-title">
+<div class="j-window-section-line l-window-section-line l-hide">
+    <div class="j-header l-header">
+        <div class="j-title l-title">
             <label><?= Language::t("common", "Линия"); ?> <span></span>
                 <select class="j-select-block">
                     <option value="0" data-id="0" data-type="0">
@@ -15,14 +15,14 @@ use system\web\Language;
                 </select>
             </label>
         </div>
-        <a href="#" class="j-remove"><?= Language::t("common", "Удалить"); ?></a>
+        <a href="#" class="j-remove l-remove"><?= Language::t("common", "Удалить"); ?></a>
     </div>
-    <div class="j-grid-stack"></div>
+    <div class="j-grid-stack l-grid-stack"></div>
 </div>
 
 <button class="j-window-section-add-line l-hide"><?= Language::t("common", "Add line"); ?></button>
 
-<div class="j-window-section-grid-stack-item">
-    <a href="#" class="j-remove">x</a>
+<div class="j-window-section-grid-stack-item l-grid-stack-item">
+    <a href="#" class="j-remove l-remove">x</a>
     <div class="j-content"></div>
 </div>
