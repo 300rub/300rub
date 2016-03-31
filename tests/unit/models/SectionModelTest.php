2341c8d7decb840edc0f6f95c68a300484c6c56d
Mikhail Vasilev <vasilev_igorevich@thbs.com>
2016-03-31 17:24:44 +0100

    .

--- tests/unit/models/SectionModelTest.php
+++ tests/unit/models/SectionModelTest.php
@@ -19,4 +19,53 @@ class SectionModelTest extends AbstractModelTest
 	{
 		$this->assertEquals(1, SectionModel::model()->byUrl("texts")->find()->id);
 	}
+
+	/**
+	 * CRUD
+	 *
+	 * @param array $createData
+	 * @param array $createExpected
+	 * @param array $createErrors
+	 * @param array $updateData
+	 * @param array $updateExpected
+	 * @param array $updateErrors
+	 *
+	 * @dataProvider dataProviderForCRUD
+	 */
+	public function testCRUD($createData, $createExpected, $createErrors, $updateData, $updateExpected, $updateErrors)
+	{
+		$model = new SectionModel;
+		$model->setAttributes($createData);
+		$this->assertEquals(true, $model->save());
+	}
+
+	public function dataProviderForCRUD()
+	{
+		return [
+			[
+				[
+					"t.seo_id"          => 1,
+					"t.language"        => 1,
+					"t.width"           => 980,
+					"t.is_main"         => 1,
+					"t.design_block_id" => 9
+				],
+				[
+					"t.seo_id"          => 1,
+					"t.language"        => 1,
+					"t.width"           => 980,
+					"t.is_main"         => 1,
+					"t.design_block_id" => 9
+				],
+				[],
+				[
+					"t.width"  => 980,
+				],
+				[
+					"t.width"  => 980,
+				],
+				[],
+			]
+		];
+	}
 }
