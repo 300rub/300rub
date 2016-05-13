6bf619f39855ddceed4b1dd1368640a8d6bf43b3
Mikhail Vasilev <donvasilion@gmail.com>
2016-05-14 00:50:04 +0100

    .

--- views/templates/window/window.login.php
+++ views/templates/window/window.login.php
@@ -4,17 +4,21 @@ use components\Language;
 <div class="j-window-login-container">
 	<div>
 		<label><?= Language::t("user", "login") ?>
-			<input class="j-t__login j-validate" type="text"/>
+			<input class="j-t__login j-validate l-form" type="text"/>
 		</label>
 	</div>
 	<div>
 		<label><?= Language::t("user", "password") ?>
-			<input class="j-t__password j-validate" type="password"/>
+			<input class="j-t__password j-validate l-form" type="password"/>
 		</label>
 	</div>
-	<div>
+	<div class="l-checkbox-container">
 		<label>
 			<input class="j-t__is_remember" type="checkbox"/>
+			<span>
+				<i class="fa fa-square-o"></i>
+				<i class="fa fa-check-square-o"></i>
+			</span>
 			<?= Language::t("user", "remember") ?>
 		</label>
 	</div>
