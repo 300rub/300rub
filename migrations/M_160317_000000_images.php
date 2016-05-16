00976e384467f58e11d42b6ce890ecc49db0cc30
Mikhail Vasilev <vasilev_igorevich@thbs.com>
2016-05-16 09:33:32 +0100

    .

--- migrations/M_160317_000000_images.php
+++ migrations/M_160317_000000_images.php
@@ -19,14 +19,28 @@ class M_160317_000000_images extends AbstractMigration
      */
     public function up()
     {
+        return
+            $this->_createImages() === true
+            &&  $this->_createImageAlbums() === true
+            && $this->_createImageInstances() === true;
+    }
+
+    /**
+     * Creates "images" table
+     *
+     * @return bool
+     */
+    private function _createImages()
+    {
         $result = $this->createTable(
             "images",
             [
-                "id"               => "pk",
-                "name"             => "string",
-                "language"         => "integer",
-                "design_block_id"  => "integer",
-                "is_keep_original" => "boolean",
+                "id"                    => "pk",
+                "name"                  => "string",
+                "language"              => "integer",
+                "design_block_id"       => "integer",
+                "design_image_block_id" => "integer",
+                "is_keep_original"      => "boolean",
             ],
             "ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci"
         );
@@ -36,7 +50,20 @@ class M_160317_000000_images extends AbstractMigration
         if (!$this->createIndex("images_design_block_id", "images", "design_block_id")) {
             return false;
         }
+        if (!$this->createIndex("images_design_image_block_id", "images", "design_image_block_id")) {
+            return false;
+        }
 
+        return true;
+    }
+
+    /**
+     * Creates "image_albums" table
+     *
+     * @return bool
+     */
+    private function _createImageAlbums()
+    {
         $result = $this->createTable(
             "image_albums",
             [
@@ -54,6 +81,16 @@ class M_160317_000000_images extends AbstractMigration
             return false;
         }
 
+        return true;
+    }
+
+    /**
+     * Creates "image_instances" table
+     *
+     * @return bool
+     */
+    private function _createImageInstances()
+    {
         $result = $this->createTable(
             "image_instances",
             [
