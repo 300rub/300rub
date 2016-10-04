32999d44615d68f6e7f027652c5fcdab26ef1009
Mikhail Vasilev <vasilev_igorevich@thbs.com>
2016-10-04 15:17:02 +0100

    .

--- commands/ClearDbCommand.php
+++ commands/ClearDbCommand.php
@@ -27,38 +27,58 @@ class ClearDbCommand extends AbstractCommand
         $config = App::getApplication()->config;
 
         $db = $config->db;
-        if (!Db::setPdo($db->host, $db->user, $db->password, $db->name)) {
-            throw new MigrationException(
-                "Unable to connect with DB for applying migrations
-					with host: {host}, user: {user}, password: {password}, name: {name}",
-                [
-                    "host"     => $db->host,
-                    "user"     => $db->user,
-                    "password" => $db->password,
-                    "name"     => $db->name,
-                ]
-            );
-        }
 
-        $tables = [];
+//        $command = 'mysql -u root -proot -h 127.0.0.1 -e "GRANT ALL PRIVILEGES ON project.* TO \'root\'@\'%\' IDENTIFIED BY \'root\'"';
+//        exec($command);
+//
+//
+//        $command = 'mysql -u root -proot -h 127.0.0.1 -e "CREATE DATABASE project"';
+//        exec($command);
 
-        $rows = Db::fetchAll("SHOW TABLES FROM " . $db->name);
-        foreach ($rows as $row) {
-            foreach ($row as $key => $value) {
-                $tables[] = $value;
-            }
-        }
+//        if (!Db::execute("DROP DATABASE IF EXISTS {$db->name}")) {
+//            throw new MigrationException("Unable to delete DB");
+//        }
+//
+//        if (!Db::execute("CREATE DATABASE IF NOT EXISTS ")) {
+//            throw new MigrationException("Unable to create DB");
+//        }
+//
+//        if (!Db::execute("GRANT ALL PRIVILEGES ON {$db->name}.* TO '{$db->user}'@'%' IDENTIFIED BY '{$db->pass}'")) {
+//            throw new MigrationException("Unable to create DB");
+//        }
 
-        foreach ($tables as $table) {
-            if (!Db::execute("DROP" . " TABLE `{$table}`")) {
-                throw new MigrationException(
-                    "Unable to delete table: {table} from DB: {db}",
-                    [
-                        "table" => $table,
-                        "db"    => $db->name
-                    ]
-                );
-            }
-        }
+//        if (!Db::setPdo($db->host, $db->user, $db->password, $db->name)) {
+//            throw new MigrationException(
+//                "Unable to connect with DB for applying migrations
+//					with host: {host}, user: {user}, password: {password}, name: {name}",
+//                [
+//                    "host"     => $db->host,
+//                    "user"     => $db->user,
+//                    "password" => $db->password,
+//                    "name"     => $db->name,
+//                ]
+//            );
+//        }
+//
+//        $tables = [];
+//
+//        $rows = Db::fetchAll("SHOW TABLES FROM " . $db->name);
+//        foreach ($rows as $row) {
+//            foreach ($row as $key => $value) {
+//                $tables[] = $value;
+//            }
+//        }
+//
+//        foreach ($tables as $table) {
+//            if (!Db::execute("DROP" . " TABLE `{$table}`")) {
+//                throw new MigrationException(
+//                    "Unable to delete table: {table} from DB: {db}",
+//                    [
+//                        "table" => $table,
+//                        "db"    => $db->name
+//                    ]
+//                );
+//            }
+//        }
 	}
 }
