6ab327a1685f37085fdd467551ce97ef39840904
Mikhail Vasilev <vasilev_igorevich@thbs.com>
2017-02-15 10:41:52 +0000

    .

---
+++ controllers/FeedbackController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace testS\controllers;
+
+/**
+ * FeedbackController
+ *
+ * @package testS\controllers
+ */
+class FeedbackController extends AbstractController
+{
+
+    /**
+     * Gets block's HTML
+     */
+    public function getHtml()
+    {
+        // TODO
+    }
+    
+    /**
+     * Gets a list of blocks
+     */
+    public function getBlocks()
+    {
+        // TODO
+    }
+
+    /**
+     * Adds block
+     */
+    public function addBlock()
+    {
+        // TODO
+    }
+
+    /**
+     * Updates block
+     */
+    public function updateBlock()
+    {
+        // TODO
+    }
+
+    /**
+     * Deletes block
+     */
+    public function deleteBlock()
+    {
+        // TODO
+    }
+
+    /**
+     * Gets block's design
+     */
+    public function getDesign()
+    {
+        // TODO
+    }
+
+    /**
+     * Updates block's design
+     */
+    public function updateDesign()
+    {
+        // TODO
+    }
+
+    /**
+     * Gets block's content
+     */
+    public function getContent()
+    {
+        // TODO
+    }
+
+    /**
+     * Updates block's content
+     */
+    public function updateContent()
+    {
+        // TODO
+    }
+}
---
+++ tests/unit/controllers/FeedbackControllerTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace testS\tests\unit\controllers;
+
+/**
+ * Tests for the controller FeedbackController
+ *
+ * @package testS\tests\unit\models
+ */
+class FeedbackControllerTest extends AbstractControllerTest
+{
+
+    public function testGetHtml()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testGetBlocks()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testAddBlock()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testUpdateBlock()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testDeleteBlock()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testGetDesign()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testUpdateDesign()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testGetContent()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function testUpdateContent()
+    {
+        $this->markTestSkipped();
+    }
+}
