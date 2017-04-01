299a74e0972d1da554b1becf9a6594041786ee94
Mikhail Vasilev <donvasilion@gmail.com>
2017-04-01 18:54:28 +0100

    .

--- public/js/TestS.js
+++ public/js/TestS.js
@@ -7,9 +7,26 @@
      * @type {Object}
      */
     window.TestS = {
+
+        /**
+         * Ajax wrapper
+         *
+         * @var {Object}
+         */
         $_ajaxWrapper: null,
+
+        /**
+         * Templates
+         *
+         * @var {Object}
+         */
         $_templates: null,
 
+        /**
+         * Gets ajax wrapper
+         *
+         * @returns {Object}
+         */
         getAjaxWrapper: function () {
             if (this.$_ajaxWrapper === null) {
                 this.$_ajaxWrapper = $("#ajax-wrapper");
@@ -17,6 +34,11 @@
             return this.$_ajaxWrapper;
         },
 
+        /**
+         * Gets templates
+         *
+         * @returns {Object}
+         */
         getTemplates: function () {
             if (this.$_templates === null) {
                 this.$_templates = $("#templates");
@@ -24,6 +46,13 @@
             return this.$_templates;
         },
 
+        /**
+         * Appends to ajax wrapper
+         *
+         * @param {Object} $object
+         *
+         * @returns {TestS}
+         */
         appendToAjaxWrapper: function ($object)
         {
             this.getAjaxWrapper().append($object);
