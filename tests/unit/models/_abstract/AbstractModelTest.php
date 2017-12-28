37029259091791bacab7550f49a2b09db458a33b
Mikhail Vasilev <donvasilion@gmail.com>
2017-12-28 00:35:21 +0000

    335.5

---
+++ tests/unit/models/_abstract/AbstractModelTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace testS\tests\unit\models\_abstract;
+
+use testS\tests\unit\_abstract\AbstractUnitTest;
+
+/**
+ * Abstract class to work with model tests
+ */
+abstract class AbstractModelTest extends AbstractUnitTest
+{
+}
