ef02d9ff2e4a2906152674c302813aa066e92459
Mikhail Vasilev <donvasilion@gmail.com>
2018-02-02 22:56:36 +0000

    .

--- fixtures/dev/_fileMap.php
+++ fixtures/dev/_fileMap.php
@@ -1,40 +1,4 @@
 <?php
 
 return [
-    [
-        'group'      => 'image',
-        'controller' => 'image',
-        'file'       => 'bigImage.jpg',
-        'data'       => [
-            'blockId'      => 3,
-            'imageGroupId' => 1
-        ],
-    ],
-    [
-        'group'      => 'image',
-        'controller' => 'image',
-        'file'       => 'bigImage.jpg',
-        'data'       => [
-            'blockId'      => 3,
-            'imageGroupId' => 1
-        ],
-    ],
-    [
-        'group'      => 'image',
-        'controller' => 'image',
-        'file'       => 'bigImage.png',
-        'data'       => [
-            'blockId'      => 4,
-            'imageGroupId' => 4
-        ],
-    ],
-    [
-        'group'      => 'image',
-        'controller' => 'image',
-        'file'       => 'bigImage.jpg',
-        'data'       => [
-            'blockId'      => 5,
-            'imageGroupId' => 4
-        ],
-    ],
 ];
