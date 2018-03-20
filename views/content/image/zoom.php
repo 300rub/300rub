a2f817ccc565337d9a724251e004e46ef2510a72
Mikhail Vasilev <mikhail.vasilev@ee.co.uk>
2018-03-20 16:56:42 +0000

    .

--- views/content/image/zoom.php
+++ views/content/image/zoom.php
@@ -11,13 +11,19 @@ echo sprintf('<div class="block-%s">', $blockId);
 
 foreach ($images as $image) {
     echo sprintf(
-        '<a data-fancybox="image-group-%s" href="%s"><img src="%s" alt="%s" title="%s" /></a>',
+        '<a data-fancybox="image-group-%s" href="%s">',
         $image->get('imageGroupId'),
-        $image->get('viewFileModel')->getUrl(),
+        $image->get('viewFileModel')->getUrl()
+    );
+
+    echo sprintf(
+        '<img src="%s" alt="%s" title="%s" />',
         $image->get('thumbFileModel')->getUrl(),
         $image->get('alt'),
         $image->get('alt')
     );
+
+    echo '</a>';
 }
 
 echo '</div>';
