ca03d9b752c1a9255082064655f27efa815109f3
Mikhail Vasilev <mikhail.vasilev@ee.co.uk>
2018-04-19 10:00:27 +0100

    .

--- models/blocks/record/RecordInstanceModel.php
+++ models/blocks/record/RecordInstanceModel.php
@@ -2,6 +2,7 @@
 
 namespace ss\models\blocks\record;
 
+use ss\application\components\Db;
 use ss\models\blocks\record\_base\AbstractRecordInstanceModel;
 
 /**
@@ -9,4 +10,58 @@ use ss\models\blocks\record\_base\AbstractRecordInstanceModel;
  */
 class RecordInstanceModel extends AbstractRecordInstanceModel
 {
+
+    /**
+     * Gets new model
+     *
+     * @return RecordInstanceModel
+     */
+    public static function model()
+    {
+        return new self;
+    }
+
+    /**
+     * Find by URL
+     *
+     * @param string $url URL
+     *
+     * @return RecordInstanceModel
+     */
+    public function byUrl($url)
+    {
+        $this->getDb()->addJoin(
+            'seo',
+            'seo',
+            Db::DEFAULT_ALIAS,
+            'seoId'
+        );
+
+        $this->getDb()->addWhere(
+            'seo.url = :url'
+        );
+        $this->getDb()->addParameter('url', $url);
+
+        return $this;
+    }
+
+    /**
+     * Adds recordId condition to SQL request
+     *
+     * @param int $recordId Record ID
+     *
+     * @return RecordInstanceModel
+     */
+    public function byRecordId($recordId)
+    {
+        $this->getDb()->addWhere(
+            sprintf(
+                '%s.recordId = :recordId',
+                Db::DEFAULT_ALIAS
+            )
+        );
+        $this->getDb()->addParameter('recordId', (int)$recordId);
+
+        return $this;
+    }
 }
--- models/blocks/record/RecordModel.php
+++ models/blocks/record/RecordModel.php
@@ -2,6 +2,8 @@
 
 namespace ss\models\blocks\record;
 
+use ss\application\App;
+use ss\application\exceptions\NotFoundException;
 use ss\models\blocks\record\_base\AbstractRecordModel;
 
 /**
@@ -16,13 +18,86 @@ class RecordModel extends AbstractRecordModel
     const CLASS_NAME = '\\ss\\models\\blocks\\record\\RecordModel';
 
     /**
+     * Page number
+     *
+     * @var int
+     */
+    private $_page = 0;
+
+    /**
+     * Record URI
+     *
+     * @var string
+     */
+    private $_recordUri = null;
+
+    /**
      * Generates HTML
      *
      * @return string
      */
     public function generateHtml()
     {
-        return '1111111111';
+        $this->_setParametersFromUri();
+
+        if ($this->_recordUri !== null) {
+            return $this->_getRecord();
+        }
+
+        return 'record list';
+    }
+
+    /**
+     * Gets record
+     *
+     * @return string
+     *
+     * @throws NotFoundException
+     */
+    private function _getRecord()
+    {
+        $recordInstance = RecordInstanceModel::model()
+            ->byUrl($this->_recordUri)
+            ->byRecordId($this->getContentId())
+            ->find();
+
+        if ($recordInstance === null) {
+            throw new NotFoundException(
+                'Unable to find record instance ' .
+                'with URI: {uri} and record ID: {recordId}',
+                [
+                    'uri'      => $this->_recordUri,
+                    'recordId' => $this->getContentId()
+                ]
+            );
+        }
+
+        return 'record';
+    }
+
+    /**
+     * Sets parameters from URI
+     *
+     * @return RecordModel
+     */
+    private function _setParametersFromUri()
+    {
+        $url = App::getInstance()->getSite()->getUri(2);
+        if ($url === null) {
+            return $this;
+        }
+
+        $page = (int)$url;
+        if ($url > 0
+            && (string)$page === $url
+            && $this->getContentModel()->get('useAutoload') === false
+        ) {
+            $this->_page = $page;
+            return $this;
+        }
+
+        $this->_recordUri = $url;
+        return $this;
     }
 
     /**
