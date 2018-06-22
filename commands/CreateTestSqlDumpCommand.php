0af069fc39f044d31e44b67864c278cdef8850e3
Mikhail Vasilev <mikhail.vasilev@ee.co.uk>
2018-06-22 16:05:25 +0100

    .

---
+++ commands/CreateTestSqlDumpCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ss\commands;
+
+use ss\application\App;
+use ss\commands\_abstract\AbstractCommand;
+
+/**
+ * Creates test SQL dump command
+ */
+class CreateTestSqlDumpCommand extends AbstractCommand
+{
+
+    /**
+     * Runs the command
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $site = App::getInstance()->getConfig()->getValue(['db', 'site1']);
+
+        exec(
+            sprintf(
+                'export MYSQL_PWD=%s; ' .
+                'mysqldump -u %s -h %s %s | gzip -c > %s/test.sql.gz',
+                $site['password'],
+                $site['user'],
+                $site['host'],
+                $site['name'],
+                FILES_ROOT . '/backups'
+            )
+        );
+    }
+}
---
+++ commands/LoadTestSqlDumpCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ss\commands;
+
+use ss\application\App;
+use ss\commands\_abstract\AbstractCommand;
+
+/**
+ * Loads test SQL dump command
+ */
+class LoadTestSqlDumpCommand extends AbstractCommand
+{
+
+    /**
+     * Runs the command
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $site = App::getInstance()->getConfig()->getValue(['db', 'site1']);
+
+        exec(
+            sprintf(
+                'export MYSQL_PWD=%s; ' .
+                'gunzip < %s | mysql -u %s -h %s %s',
+                $site['password'],
+                FILES_ROOT . '/backups/test.sql.gz',
+                $site['user'],
+                $site['host'],
+                $site['name']
+            )
+        );
+    }
+}
