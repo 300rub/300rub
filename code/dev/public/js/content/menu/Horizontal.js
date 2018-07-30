6e890174f8495a29a97b8dbdd7072e5555f62e6e
Mikhail Vasilev <mikhail.vasilev@ee.co.uk>
2018-07-30 13:27:17 +0100

    .

--- code/dev/public/js/content/menu/Horizontal.js
+++ code/dev/public/js/content/menu/Horizontal.js
@@ -8,6 +8,9 @@
      */
     ss.content.menu.Horizontal = function (options) {
         ss.content.menu.Abstract.call(this, options);
+
+        this._width = 0;
+
         this.init();
     };
 
@@ -28,6 +31,63 @@
      * Init
      */
     ss.content.menu.Horizontal.prototype.init = function () {
-        console.log(this.getMenu());
+        $(window).resize(
+            $.proxy(
+                function () {
+                    this._setMenu();
+                },
+                this
+            )
+        );
+
+        this._setMenu();
     };
+
+    /**
+     * Sets menu
+     *
+     * @private
+     */
+    ss.content.menu.Horizontal.prototype._setMenu = function() {
+        this._width = this.getMenu().width();
+        this._setRight(this.getMenu().find("> ul"), 0);
+    };
+
+    /**
+     * Sets right class for overlaid menu items
+     *
+     * @param {Object}  object
+     * @param {integer} leftPosition
+     *
+     * @private
+     */
+    ss.content.menu.Horizontal.prototype._setRight = function(
+        object,
+        leftPosition
+    ) {
+        var t = this;
+
+        object.find("> li").each(function() {
+            var liWidth = $(this).width();
+            var liLeft = $(this).position().left;
+            var ulElement = $(this).find("> ul");
+            var ulWidth = ulElement.width();
+            var rightValue = leftPosition + liLeft + liWidth + ulWidth;
+            var left = 0;
+
+            if (rightValue > t._width) {
+                ulElement.addClass("right");
+                if (leftPosition > 0) {
+                    left = leftPosition + liLeft - ulWidth;
+                } else {
+                    left = liLeft + liWidth - ulWidth;
+                }
+            } else {
+                left = leftPosition + liLeft + liWidth;
+            }
+
+            t._setRight(ulElement, left);
+        });
+    }
+
 }(window.jQuery, window.ss);
