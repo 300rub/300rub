7b608dea44c539964db4b1a627d8c746674ba835
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-08-14 23:09:39 +0100

    .

--- code/dev/views/site/create.php
+++ code/dev/views/site/create.php
@@ -1,16 +1,3 @@
-<?php
-/**
- * Variables
- *
- * @var string $createSite
- */
-?>
-<div class="container">
-    <button class="btn btn-big btn-blue">
-        <span><?php echo $createSite; ?></span>
-    </button>
-</div>
-
 <script>
     $(document).ready(function() {
         new ss.window.site.Create();
