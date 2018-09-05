5bc4dac0c860364e65a8722df61f9fc6685b2b12
Mikhail Vasilev <mikhail.vasilev@ee.co.uk>
2018-09-05 10:13:17 +0100

    .

--- code/dev/controllers/section/UpdateStructureController.php
+++ code/dev/controllers/section/UpdateStructureController.php
@@ -2,22 +2,30 @@
 
 namespace ss\controllers\section;
 
-use ss\application\App;
-use ss\application\components\db\Table;
 use ss\application\components\user\Operation;
-use ss\application\exceptions\NotFoundException;
 use ss\controllers\_abstract\AbstractController;
-use ss\models\blocks\block\BlockModel;
-use ss\models\blocks\record\RecordCloneModel;
 use ss\models\sections\GridLineModel;
 use ss\models\sections\GridModel;
-use ss\models\sections\SectionModel;
 
 /**
  * Updates section structure
  */
 class UpdateStructureController extends AbstractController
 {
+
+    /**
+     * New line IDs
+     *
+     * @var int[]
+     */
+    private $_newLineIds = [];
+
+    /**
+     * Current lines
+     *
+     * @var GridLineModel[]
+     */
+    private $_currentLines = [];
 
     /**
      * Runs controller
@@ -37,8 +45,144 @@ class UpdateStructureController extends AbstractController
             ]
         );
 
-
+        $this
+            ->_setNewLineIds()
+            ->_setCurrentLines()
+            ->_deleteExtraLines()
+            ->_deleteExtraGrids()
+            ->_updateStructure();
 
         return $this->getSimpleSuccessResult();
+    }
+
+    /**
+     * Sets new line IDs
+     *
+     * @return UpdateStructureController
+     */
+    private function _setNewLineIds()
+    {
+        $this->_newLineIds = [];
+
+        foreach ($this->get('structure') as $line) {
+            if ($line['id'] === 0) {
+                continue;
+            }
+
+            $this->_newLineIds[] = $line['id'];
+        }
+
+        return $this;
+    }
+
+    /**
+     * Sets current lines
+     *
+     * @return UpdateStructureController
+     */
+    private function _setCurrentLines()
+    {
+        $this->_currentLines = [];
+
+        $currentLines = GridLineModel::model()
+            ->bySectionId($this->get('id'))
+            ->findAll();
+
+        foreach ($currentLines as $currentLine) {
+            $this->_currentLines[$currentLine->getId()] = $currentLine;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Deletes old extra lines
+     *
+     * @return UpdateStructureController
+     */
+    private function _deleteExtraLines()
+    {
+        foreach ($this->_currentLines as $lineId => $lineModel) {
+            if (in_array($lineId, $this->_newLineIds) === true) {
+                continue;
+            }
+
+            $lineModel->delete();
+            unset($this->_currentLines[$lineId]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Updates structure
+     *
+     * @return UpdateStructureController
+     */
+    private function _updateStructure()
+    {
+        $sort = 10;
+
+        foreach ($this->get('structure') as $line) {
+            $lineModel = $this->_getLineById($line['id']);
+            $lineModel->set(['sort' => $sort]);
+            $lineModel->save();
+
+            foreach ($line['grids'] as $grid) {
+                $gridModel = new GridModel();
+                $gridModel->set(
+                    [
+                        'gridLineId' => $lineModel->getId(),
+                        'blockId'    => $grid['blockId'],
+                        'x'          => $grid['x'],
+                        'y'          => $grid['y'],
+                        'width'      => $grid['width'],
+                    ]
+                );
+                $gridModel->save();
+            }
+
+            $sort += 10;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Gets Line model
+     *
+     * @param int $lineId LineId
+     *
+     * @return GridLineModel
+     */
+    private function _getLineById($lineId)
+    {
+        $hasLine = array_key_exists($lineId, $this->_currentLines);
+        if ($hasLine === true) {
+            return $this->_currentLines[$lineId];
+        }
+
+        $lineModel = new GridLineModel();
+        $lineModel->set(['sectionId' => $this->get('id')]);
+        return $lineModel;
+    }
+
+    /**
+     * Deletes old grids
+     *
+     * @return UpdateStructureController
+     */
+    private function _deleteExtraGrids()
+    {
+        foreach (array_keys($this->_currentLines) as $lineId) {
+            GridModel::model()->delete(
+                'gridLineId = :gridLineId',
+                [
+                    'gridLineId' => $lineId
+                ]
+            );
+        }
+
+        return $this;
     }
 }
