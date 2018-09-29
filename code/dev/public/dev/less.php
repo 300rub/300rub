76711e96e2958ac77bd6730d7e3f694cb696b413
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-09-29 20:57:32 +0100

    .

--- code/dev/public/dev/less.php
+++ code/dev/public/dev/less.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 'On');
+
 require __DIR__ . '/../../vendor/leafo/lessphp/lessc.inc.php';
 require __DIR__ . '/../../application/components/file/Less.php';
 
