c812370180f269d64facd3497f6107d6b9bb5205
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-10-10 21:14:38 +0100

    .

--- code/dev/public/static/admin/block/image/content/js.js
+++ code/dev/public/static/admin/block/image/content/js.js
@@ -41,16 +41,97 @@ ss.add(
          * On load success
          */
         onLoadSuccess: function() {
+            if (this.getData("useAlbums") === false) {
+                this.setImages();
+            } else {
+                this.setAlbums();
+            }
 
+            this.setSubmit(
+                {
+                    label: this.getLabel("button"),
+                    icon: "fas fa-save",
+                    ajax: {
+                        data: {
+                            group: "image",
+                            controller: "content",
+                            data: {
+                                id: this.getData("id")
+                            }
+                        },
+                        type: "PUT",
+                        success: $.proxy(this.onSendSuccess, this)
+                    }
+                }
+            );
+        },
+
+        /**
+         * Sets images
+         */
+        setImages: function () {
+            ss.init(
+                "adminBlockImageImages",
+                {
+                    appendTo: this.getBody(),
+                    isSortable: true,
+                    list: this._data.list,
+                    create: {
+                        hasOperation: this.getData("canCreate"),
+                        isSingleton: false,
+                        group: "image",
+                        controller: "image",
+                        data: {
+                            blockId: this.getData("id"),
+                            imageGroupId: this.getData("groupId")
+                        }
+                    },
+                    update: {
+                        hasOperation: this.getData("canUpdate"),
+                        blockId: this.getData("id"),
+                        level: 2,
+                        parent: "image-content"
+                    },
+                    delete: {
+                        hasOperation: this.getData("canDelete"),
+                        group: "image",
+                        controller: "image",
+                        data: {
+                            blockId: this.getData("id")
+                        },
+                        confirm: {
+                            text: this.getLabel("deleteConfirm"),
+                            yes: this.getLabel("delete"),
+                            no: this.getLabel("no")
+                        }
+                    }
+                }
+            );
+        },
+
+        /**
+         * Sets albums
+         */
+        setAlbums: function () {
+            //
         },
 
         /**
          * On send success
-         *
-         * @param {Object} data
          */
-        onSendSuccess: function (data) {
-            console.log(data);
+        onSendSuccess: function () {
+            this.remove();
+
+            if (this.getOption("blockId") !== 0) {
+                ss.init(
+                    "commonContentBlockUpdate",
+                    {
+                        list: [
+                            this.getOption("blockId")
+                        ]
+                    }
+                );
+            }
         }
     }
 );
