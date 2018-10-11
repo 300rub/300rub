0af8150722eaa1a00cada24ee410b963d97aeda5
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-10-12 00:28:21 +0100

    .

---
+++ code/dev/public/static/admin/components/design/block/background/js.js
@@ -0,0 +1,23 @@
+!function ($, ss) {
+    "use strict";
+
+    var name = "adminComponentsDesignBlockBackground";
+
+    var parameters = {
+        /**
+         * Parent
+         *
+         * @var {String}
+         */
+        parent: "",
+
+        /**
+         * Init
+         */
+        init: function () {
+
+        }
+    };
+
+    ss.add(name, parameters);
+}(window.jQuery, window.ss);
