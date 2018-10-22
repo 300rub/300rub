b05b76b668007fd860c4b1efe0972a32a9f2b221
Mikhail Vasilev <mikhail.vasilev@ee.co.uk>
2018-10-22 17:26:20 +0100

    .

--- code/dev/public/static/admin/block/image/content/js.js
+++ code/dev/public/static/admin/block/image/content/js.js
@@ -116,36 +116,182 @@
          * Sets albums
          */
         setAlbums: function () {
-            ss.init(
-                "adminBlockImageAlbums",
-                {
-                    blockId: this.getOption("blockId"),
-                    appendTo: this.getBody(),
-                    list: this.getData("list", {}),
-                    isSortable: true,
-                    images: {
-                        label: this.getLabel("images")
-                    },
-                    create: {
-                        hasOperation: this.getData("canCreate"),
-                        label: this.getLabel("addAlbum"),
-                        appendTo: this.getFooter()
-                    },
-                    update: {
-                        hasOperation: this.getData("canUpdate"),
-                        label: this.getLabel("edit")
-                    },
-                    remove: {
-                        hasOperation: this.getData("canDelete"),
-                        label: this.getLabel("delete"),
-                        confirm: {
-                            text: this.getLabel("deleteConfirm"),
-                            yes: this.getLabel("delete"),
-                            no: this.getLabel("no")
+            this
+                .setAlbumList()
+                .setAddAlbumButton();
+        },
+
+        /**
+         * Sets album list
+         */
+        setAlbumList: function () {
+            var groupContainer
+                = ss.init("template").get("image-group-sort-container");
+
+            groupContainer.appendTo(this.getBody());
+
+            $.each(
+                this.getData("list", {}),
+                $.proxy(
+                    function (i, itemData) {
+                        if (itemData.id === 0) {
+                            return this;
                         }
+
+                        var itemElement = ss.init("template").get(
+                            "image-group-sort-item"
+                        );
+
+                        var coverContainer
+                            = itemElement.find(".cover-container");
+                        if (itemData.cover === null) {
+                            coverContainer.remove();
+                        } else {
+                            coverContainer
+                                .find(".cover")
+                                .attr("src", itemData.cover.url);
+                        }
+
+                        itemElement.find(".title").text(itemData.name);
+
+                        this.setAlbumButtons(itemElement, itemData);
+
+                        groupContainer.append(itemElement);
+                    },
+                    this
+                )
+            );
+
+            if (this.getOption("isSortable") === true) {
+                this.container.sortable(
+                    {
+                        items: ".image-group-sort-item"
                     }
+                );
+            }
+
+            return this;
+        },
+
+        /**
+         * Sets add album button
+         */
+        setAddAlbumButton: function () {
+            if (this.getData("canCreate") !== true) {
+                return this;
+            }
+
+            ss.init(
+                "commonComponentsFormButton",
+                {
+                    css: "btn btn-gray",
+                    icon: "fas fa-plus",
+                    label: this.getLabel("addAlbum"),
+                    appendTo: this.getFooter(),
+                    onClick: $.proxy(function() {
+                        ss.init(
+                            "adminBlockImageAlbumAdd",
+                            {
+                                blockId: this.getOption("blockId"),
+                            }
+                        );
+                    }, this)
                 }
             );
+
+            return this;
+        },
+
+        /**
+         * Sets album buttons
+         *
+         * @param {Object} itemElement
+         * @param {Object} itemData
+         */
+        setAlbumButtons: function (itemElement, itemData) {
+            var buttons = itemElement.find(".buttons");
+
+            ss.init(
+                "commonComponentsFormButton",
+                {
+                    css: "btn btn-gray",
+                    icon: "fas fa-images",
+                    label: this.getLabel("images"),
+                    appendTo: buttons,
+                    onClick: $.proxy(
+                        function () {
+                            ss.init(
+                                "adminBlockImageContent",
+                                {
+                                    groupId: itemData.id,
+                                    blockId: this.getOption("blockId")
+                                }
+                            );
+                        },
+                        this
+                    )
+                }
+            );
+
+            if (this.getData("canUpdate") === true) {
+                ss.init(
+                    "commonComponentsFormButton",
+                    {
+                        css: "btn btn-blue",
+                        icon: "fas fa-edit",
+                        label: this.getLabel("edit"),
+                        appendTo: buttons,
+                        onClick: $.proxy(
+                            function () {
+                                ss.init(
+                                    "adminBlockImageAlbumUpdate",
+                                    {
+                                        groupId: itemData.id,
+                                        blockId: this.getOption("blockId")
+                                    }
+                                );
+                            },
+                            this
+                        )
+                    }
+                );
+            }
+
+            if (this.getData("canDelete") === true) {
+                ss.init(
+                    "commonComponentsFormButton",
+                    {
+                        css: "btn btn-red",
+                        icon: "fas fa-trash",
+                        label: this.getLabel("delete"),
+                        appendTo: buttons,
+                        confirm: {
+                            text: this.getLabel("deleteConfirm"),
+                            yes: {
+                                label: this.getLabel("delete"),
+                                icon: "fas fa-trash"
+                            },
+                            no: this.getLabel("no")
+                        },
+                        ajax: {
+                            data: {
+                                group: "image",
+                                controller: "album",
+                                data: {
+                                    id: itemData.id,
+                                    blockId: this.getOption("blockId")
+                                }
+                            },
+                            type: "DELETE",
+                            success: function () {
+                                itemElement.remove();
+                            }
+                        }
+                    }
+                );
+            }
+
+            return this;
         },
 
         /**
