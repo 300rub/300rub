b5a0314bdd45ddda7182d683d5551ac6b11f2453
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-11-01 23:14:03 +0000

    .

--- code/config/common.php
+++ code/config/common.php
@@ -1,16 +1,22 @@
 <?php
 
-define('APP_ENV', getenv('APP_ENV'));
 define('ENV_DEV', 'dev');
 define('ENV_PROD', 'prod');
+define('ENV_DEV_PROD', 'dev_prod');
+
 define('CODE_ROOT', __DIR__ . '/..');
 define('FILES_ROOT', __DIR__ . '/../..');
 
-switch (APP_ENV) {
+switch (getenv('APP_ENV')) {
     case ENV_DEV:
+        define('APP_ENV', ENV_DEV);
         return include 'dev.php';
     case ENV_PROD:
+        define('APP_ENV', ENV_PROD);
         return include 'prod.php';
+    case ENV_DEV_PROD:
+        define('APP_ENV', ENV_PROD);
+        return include 'dev.php';
     default:
         throw new Exception(sprintf('Undefined env: [%s]', APP_ENV));
 }
