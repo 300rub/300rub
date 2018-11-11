10483807d9953c13217f6da1ed6de501a8bbde8b
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-11-11 10:40:04 +0000

    .

--- code/config/common.php
+++ code/config/common.php
@@ -11,12 +11,10 @@ switch (getenv('APP_ENV')) {
     case ENV_DEV:
         define('APP_ENV', ENV_DEV);
         return include 'dev.php';
-    case ENV_PROD:
-        define('APP_ENV', ENV_PROD);
-        return include 'prod.php';
     case ENV_DEV_PROD:
         define('APP_ENV', ENV_PROD);
         return include 'dev.php';
     default:
-        throw new Exception(sprintf('Undefined env: [%s]', APP_ENV));
+        define('APP_ENV', ENV_PROD);
+        return include 'prod.php';
 }
