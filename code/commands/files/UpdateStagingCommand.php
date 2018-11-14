9618be3bcb78eb86fc93d54b6aefed7fbc6925f0
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-11-14 20:53:55 +0000

    .

---
+++ code/commands/files/UpdateStagingCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ss\commands\files;
+
+use Aws\Ssm\SsmClient;
+use ss\application\App;
+use ss\commands\_abstract\AbstractCommand;
+
+/**
+ *  Command to update staging
+ */
+class UpdateStagingCommand extends AbstractCommand
+{
+
+    /**
+     * Runs the command
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $awsClient = App::getInstance()
+            ->getConfig()
+            ->getValue(['aws', 'client']);
+
+        $ssmClient = new SsmClient(
+            [
+                'profile' => $awsClient['profile'],
+                'region'  => $awsClient['region'],
+                'version' => $awsClient['version'],
+            ]
+        );
+
+        $result = $ssmClient->sendCommand(
+            [
+
+            ]
+        );
+
+        var_dump($result);
+    }
+}
