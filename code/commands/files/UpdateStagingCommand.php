91d384c95a633c03a1475b1d50ff85db98e76e18
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-11-14 20:57:48 +0000

    .

--- code/commands/files/UpdateStagingCommand.php
+++ code/commands/files/UpdateStagingCommand.php
@@ -33,7 +33,7 @@ class UpdateStagingCommand extends AbstractCommand
 
         $result = $ssmClient->sendCommand(
             [
-
+                'DocumentName' => 'AWS-RunRemoteScript'
             ]
         );
 
