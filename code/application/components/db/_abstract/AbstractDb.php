f844be002e8cb9a25e6b866d656a43e99bcbd736
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-11-17 18:55:20 +0000

    .

--- code/application/components/db/_abstract/AbstractDb.php
+++ code/application/components/db/_abstract/AbstractDb.php
@@ -116,7 +116,8 @@ abstract class AbstractDb
             [
                 \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'',
+                \PDO::ATTR_AUTOCOMMIT         => 0,
             ]
         );
 
