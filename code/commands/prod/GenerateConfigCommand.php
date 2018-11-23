2d9902332a11983e6e658a180bad69f47b8caf5a
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-11-23 23:27:33 +0000

    .

---
+++ code/commands/prod/GenerateConfigCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace ss\commands\prod;
+
+use Aws\Ssm\SsmClient;
+use ss\application\App;
+use ss\application\exceptions\FileException;
+use ss\commands\_abstract\AbstractCommand;
+
+/**
+ *  Command to generate config file
+ */
+class GenerateConfigCommand extends AbstractCommand
+{
+
+    /**
+     * Runs the command
+     *
+     * @return void
+     *
+     * @throws FileException
+     */
+    public function run()
+    {
+        $file = CODE_ROOT . '/config/env/prod.json';
+        $content = file_get_contents($file);
+
+        preg_match_all('/\{\{[a-zA-Z]+\}\}/', $content, $matches);
+        $matches = $matches[0];
+
+        $names = [];
+        foreach ($matches as $match) {
+            $names[] = str_replace(['{', '}'], '', $match);
+        }
+
+        $awsClient = App::getInstance()
+            ->getConfig()
+            ->getValue(['aws', 'client']);
+
+        $ssmClient = new SsmClient(
+            [
+                'profile' => $awsClient['profile'],
+                'region'  => $awsClient['region'],
+                'version' => $awsClient['version'],
+            ]
+        );
+
+        $result = $ssmClient->getParameters(
+            [
+                'Names' => $names,
+            ]
+        );
+
+        $nameValueList = [];
+        foreach ($result['Parameters'] as $parameter) {
+            $nameValueList[$parameter['Name']] = $parameter['Value'];
+        }
+
+        foreach ($matches as $match) {
+            $name = str_replace(['{', '}'], '', $match);
+            if (array_key_exists($name, $nameValueList) === false) {
+                continue;
+            }
+
+            $content = str_replace($match, $nameValueList[$name], $content);
+        }
+
+        var_dump($content);
+    }
+}
