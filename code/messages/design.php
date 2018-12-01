942cd94e19365f78111aa0a90be936717269565a
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-12-01 11:20:37 +0000

    .

--- code/messages/design.php
+++ code/messages/design.php
@@ -21,79 +21,80 @@ return [
     ],
     'gradientDirection'     => [
         Language::LANGUAGE_EN_ID => 'Gradient direction',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Направление градиента',
     ],
     'gradientDirectionHover'     => [
         Language::LANGUAGE_EN_ID => 'Gradient direction on mouse-over',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Направление градиента при наведении мыши',
     ],
     'blockDesign'     => [
         Language::LANGUAGE_EN_ID => 'Block design',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Дизайн блока',
     ],
     'textDesign'     => [
         Language::LANGUAGE_EN_ID => 'Text design',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Дизайн текста',
     ],
     'mouseHoverEffect'     => [
         Language::LANGUAGE_EN_ID => 'Mouse-over effect',
-        Language::LANGUAGE_RU_ID => ''
+        Language::LANGUAGE_RU_ID => 'Эффект при наведении мыши'
     ],
     'mouseHoverAnimation'     => [
         Language::LANGUAGE_EN_ID => 'Mouse-over animation',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Анимация при наведении мыши',
     ],
     'backgroundColor'     => [
         Language::LANGUAGE_EN_ID => 'Background color',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Цвет заднего фона',
     ],
     'backgroundImage'     => [
         Language::LANGUAGE_EN_ID => 'Background image',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Картинка на заднем плане',
     ],
     'borderColor'     => [
         Language::LANGUAGE_EN_ID => 'Border color',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Цвет границы',
     ],
     'borderColorHover'     => [
         Language::LANGUAGE_EN_ID => 'Border color on mouse-over',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Цвет границы при наведении мыши',
     ],
     'useGradient'     => [
         Language::LANGUAGE_EN_ID => 'Use gradient',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Использовать градиент',
     ],
     'borderStyle'     => [
         Language::LANGUAGE_EN_ID => 'Border style',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Стиль границы',
     ],
     'borderStyleHover'     => [
         Language::LANGUAGE_EN_ID => 'Border style on mouse-over',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Стиль границы при наведении мыши',
     ],
     'textSample' => [
         Language::LANGUAGE_EN_ID
             => 'All their equipment and instruments are alive.',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID
+            => 'Съешь еще мягких французских булок.',
     ],
     'imagesContainer' => [
         Language::LANGUAGE_EN_ID => 'Images container',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Контейнер изображения',
     ],
     'imageBlock' => [
         Language::LANGUAGE_EN_ID => 'Image block',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Блок изображения',
     ],
     'image' => [
         Language::LANGUAGE_EN_ID => 'Image',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Изображение',
     ],
     'navigationBlock' => [
         Language::LANGUAGE_EN_ID => 'Navigation block',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Блок навигации',
     ],
     'descriptionBlock' => [
         Language::LANGUAGE_EN_ID => 'Description block',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Блок описания',
     ],
 ];
