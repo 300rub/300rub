455140a322daa7c678ccdf0a984b413abe2569f5
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-12-01 11:38:47 +0000

    .

--- code/messages/payment.php
+++ code/messages/payment.php
@@ -5,11 +5,11 @@ use ss\application\components\common\Language;
 return [
     'payment'          => [
         Language::LANGUAGE_EN_ID => 'Payment',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Оплата',
     ],
     'panelDescription' => [
         Language::LANGUAGE_EN_ID
             => 'You can show your balance here and make payments',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => '...',
     ]
 ];
--- code/messages/record.php
+++ code/messages/record.php
@@ -5,82 +5,82 @@ use ss\application\components\common\Language;
 return [
     'records'            => [
         Language::LANGUAGE_EN_ID => 'Records',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Записи',
     ],
     'panelDescription'            => [
-        Language::LANGUAGE_EN_ID => 'Records panel description...',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_EN_ID => '...',
+        Language::LANGUAGE_RU_ID => '...',
     ],
     'editBlockTitle'    => [
         Language::LANGUAGE_EN_ID => 'Edit record',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Редактировать запись',
     ],
     'editCloneBlockTitle'    => [
         Language::LANGUAGE_EN_ID => 'Edit record clone',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Редактировать клон записи',
     ],
     'addBlockTitle'    => [
         Language::LANGUAGE_EN_ID => 'Add record',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Добавить запись',
     ],
     'addCloneBlockTitle'    => [
         Language::LANGUAGE_EN_ID => 'Add record clone',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Добавить клон записи',
     ],
     'editBlockDescription'    => [
-        Language::LANGUAGE_EN_ID => 'Edit block description...',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_EN_ID => '...',
+        Language::LANGUAGE_RU_ID => '...',
     ],
     'editCloneBlockDescription'    => [
-        Language::LANGUAGE_EN_ID => 'Edit block clone description...',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_EN_ID => '...',
+        Language::LANGUAGE_RU_ID => '...',
     ],
     'addBlockDescription'    => [
-        Language::LANGUAGE_EN_ID => 'Add block description...',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_EN_ID => '...',
+        Language::LANGUAGE_RU_ID => '...',
     ],
     'addCloneBlockDescription'    => [
-        Language::LANGUAGE_EN_ID => 'Add block clone description...',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_EN_ID => '...',
+        Language::LANGUAGE_RU_ID => '...',
     ],
     'hasCover'    => [
         Language::LANGUAGE_EN_ID => 'Has cover',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Обложка',
     ],
     'hasImages'    => [
         Language::LANGUAGE_EN_ID => 'Has images',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Изображения',
     ],
     'hasCoverZoom'    => [
         Language::LANGUAGE_EN_ID => 'Has cover zoom',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Увеличение обложки',
     ],
     'hasDescription'    => [
         Language::LANGUAGE_EN_ID => 'Has description',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Краткое описание',
     ],
     'useAutoload'    => [
         Language::LANGUAGE_EN_ID => 'Use autoload',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Использовать автозагрузку',
     ],
     'pageNavigationSize'    => [
         Language::LANGUAGE_EN_ID => 'Page navigation size',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Количество записей на странице',
     ],
     'shortCardDateType'    => [
         Language::LANGUAGE_EN_ID => 'Short card date type',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Тип даты для краткого описания',
     ],
     'fullCardDateType'    => [
-        Language::LANGUAGE_EN_ID => 'Short card date type',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_EN_ID => 'Full card date type',
+        Language::LANGUAGE_RU_ID => 'Тип даты для подробного описания',
     ],
     'dateType'    => [
         Language::LANGUAGE_EN_ID => 'Date type',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Тип даты',
     ],
     'maxCount'    => [
         Language::LANGUAGE_EN_ID => 'Max count',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Максимальное количество',
     ],
 ];
