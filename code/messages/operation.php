229349353566f87a904ba6d8b1f2d223e241ffca
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-12-01 11:31:11 +0000

    .

--- code/messages/operation.php
+++ code/messages/operation.php
@@ -5,11 +5,11 @@ use ss\application\components\common\Language;
 return [
     'all'          => [
         Language::LANGUAGE_EN_ID => 'All',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Все',
     ],
     'settingsChangeIcon'          => [
         Language::LANGUAGE_EN_ID => 'Change icon',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Изменить иконку',
     ],
     'settingsHiddenCode'          => [
         Language::LANGUAGE_EN_ID => 'Hidden code',
@@ -17,86 +17,86 @@ return [
     ],
     'settingsViewUsers'          => [
         Language::LANGUAGE_EN_ID => 'View users',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Просмотр пользователей',
     ],
     'settingsEditUsers'  => [
         Language::LANGUAGE_EN_ID => 'Edit users',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Редактировать пользователя',
     ],
     'settingsAddUsers'  => [
         Language::LANGUAGE_EN_ID => 'Add users',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Добавить пользователя',
     ],
     'settingsDeleteUsers'  => [
         Language::LANGUAGE_EN_ID => 'Delete users',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Удалить пользователей',
     ],
     'settingsViewUserSessions' => [
         Language::LANGUAGE_EN_ID => 'View user sessions',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Посмотреть сессии пользователя',
     ],
     'settingsDeleteUserSessions' => [
         Language::LANGUAGE_EN_ID => 'Delete user sessions',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Удалить сессии пользователя',
     ],
     'add' => [
         Language::LANGUAGE_EN_ID => 'Add',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Добавить',
     ],
     'addClone' => [
         Language::LANGUAGE_EN_ID => 'Add clone',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Добавить клон',
     ],
     'edit' => [
         Language::LANGUAGE_EN_ID => 'Edit',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Редактировать',
     ],
     'delete' => [
         Language::LANGUAGE_EN_ID => 'Delete',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Удалить',
     ],
     'deleteClone' => [
-        Language::LANGUAGE_EN_ID => 'Delete Clone',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_EN_ID => 'Delete clone',
+        Language::LANGUAGE_RU_ID => 'Удалить клон',
     ],
     'duplicate' => [
         Language::LANGUAGE_EN_ID => 'Duplicate',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Дублировать',
     ],
     'duplicateClone' => [
         Language::LANGUAGE_EN_ID => 'Duplicate Clone',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Дублировать клон',
     ],
     'editDesign' => [
         Language::LANGUAGE_EN_ID => 'Edit design',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Редактировать дизайн',
     ],
     'editSettings' => [
         Language::LANGUAGE_EN_ID => 'Edit settings',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Редактировать настройки',
     ],
     'editCloneSettings' => [
         Language::LANGUAGE_EN_ID => 'Edit clone settings',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Редактировать настройки клона',
     ],
     'imageUpload' => [
         Language::LANGUAGE_EN_ID => 'Image upload',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Загрузка изображений',
     ],
     'imageDelete' => [
         Language::LANGUAGE_EN_ID => 'Image delete',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Удаление изображений',
     ],
     'imageUpdate' => [
         Language::LANGUAGE_EN_ID => 'Image update',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Редактирование изображений',
     ],
     'imageCrop' => [
         Language::LANGUAGE_EN_ID => 'Image crop',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Обрезка изображений',
     ],
     'settingsCanRelease' => [
         Language::LANGUAGE_EN_ID => 'Can release',
-        Language::LANGUAGE_RU_ID => 'Может делать релиз',
+        Language::LANGUAGE_RU_ID => 'Возможность делать релиз',
     ]
 ];
