ddd4affdb373961e4756235c160e90a0de3fc3ea
mariiavasileva <m.vasileva2@newcastle.ac.uk>
2018-12-01 11:15:46 +0000

    .

--- code/messages/block.php
+++ code/messages/block.php
@@ -9,19 +9,19 @@ return [
     ],
     'selectCategory' => [
         Language::LANGUAGE_EN_ID => 'Please select any category',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Выберите любую категорию',
     ],
     'all' => [
         Language::LANGUAGE_EN_ID => 'all blocks',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'все блоки',
     ],
     'fromPage' => [
         Language::LANGUAGE_EN_ID => 'from page',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'со страницы',
     ],
     'blockDescription' => [
         Language::LANGUAGE_EN_ID => 'Select any block type',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Выберите любой тип',
     ],
     'buttonName' => [
         Language::LANGUAGE_EN_ID => 'Blocks',
--- code/messages/common.php
+++ code/messages/common.php
@@ -5,27 +5,27 @@ use ss\application\components\common\Language;
 return [
     'add'       => [
         Language::LANGUAGE_EN_ID => 'Add',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Добавить',
     ],
     'duplicate' => [
         Language::LANGUAGE_EN_ID => 'Duplicate',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Дублировать',
     ],
     'delete'    => [
         Language::LANGUAGE_EN_ID => 'Delete',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Удалить',
     ],
     'save'      => [
         Language::LANGUAGE_EN_ID => 'Save',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Сохранить',
     ],
     'update'      => [
         Language::LANGUAGE_EN_ID => 'Update',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Обновить',
     ],
     'edit'      => [
         Language::LANGUAGE_EN_ID => 'Edit',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Редактировать',
     ],
     'name'      => [
         Language::LANGUAGE_EN_ID => 'Name',
@@ -37,23 +37,23 @@ return [
     ],
     'url'     => [
         Language::LANGUAGE_EN_ID => 'URL',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'URL',
     ],
     'alias'     => [
         Language::LANGUAGE_EN_ID => 'Alias',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Alias',
     ],
     'title'     => [
         Language::LANGUAGE_EN_ID => 'Title',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Заголовок',
     ],
     'keywords'            => [
         Language::LANGUAGE_EN_ID => 'Keywords',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Ключевые слова',
     ],
     'description'         => [
         Language::LANGUAGE_EN_ID => 'Description',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Описание',
     ],
     'type'      => [
         Language::LANGUAGE_EN_ID => 'Type',
@@ -61,27 +61,23 @@ return [
     ],
     'settings'  => [
         Language::LANGUAGE_EN_ID => 'Settings',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Настройки',
     ],
     'design'    => [
         Language::LANGUAGE_EN_ID => 'Design',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Дизайн',
     ],
     'copy'      => [
         Language::LANGUAGE_EN_ID => 'Copy',
-        Language::LANGUAGE_RU_ID => '',
-    ],
-    'about' => [
-        Language::LANGUAGE_EN_ID => 'About',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Копировать',
     ],
     'help' => [
         Language::LANGUAGE_EN_ID => 'Help',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Помощь',
     ],
     'seo' => [
         Language::LANGUAGE_EN_ID => 'SEO',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'СЕО',
     ],
     'error' => [
         Language::LANGUAGE_EN_ID => 'An error occurred',
@@ -89,43 +85,43 @@ return [
     ],
     'cancel' => [
         Language::LANGUAGE_EN_ID => 'Cancel',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Отменить',
     ],
     'deleteConfirmation' => [
         Language::LANGUAGE_EN_ID => 'Do you really want to delete?',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Вы действительно хотите удалить?',
     ],
     'none' => [
         Language::LANGUAGE_EN_ID => 'None',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Отсутствует',
     ],
     'left' => [
         Language::LANGUAGE_EN_ID => 'Left',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Лево',
     ],
     'center' => [
         Language::LANGUAGE_EN_ID => 'Center',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Центр',
     ],
     'right' => [
         Language::LANGUAGE_EN_ID => 'Right',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Право',
     ],
     'open'      => [
         Language::LANGUAGE_EN_ID => 'Open',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Открыть',
     ],
     'no' => [
         Language::LANGUAGE_EN_ID => 'No',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Нет',
     ],
     'clear' => [
         Language::LANGUAGE_EN_ID => 'Clear',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Очистить',
     ],
     'notFound' => [
         Language::LANGUAGE_EN_ID => 'Page not found',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Страница не найдена',
     ],
     'address' => [
         Language::LANGUAGE_EN_ID => 'Address',
@@ -149,7 +145,7 @@ return [
     ],
     'published'                => [
         Language::LANGUAGE_EN_ID => 'Published',
-        Language::LANGUAGE_RU_ID => '',
+        Language::LANGUAGE_RU_ID => 'Опубликовано',
     ],
     'default'                => [
         Language::LANGUAGE_EN_ID => 'Default',
